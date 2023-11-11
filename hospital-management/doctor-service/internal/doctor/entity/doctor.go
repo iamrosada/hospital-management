89c628868581fa3d44e30c8a51a9dfeb7346ba0b
iamrosada <luisrosada2018@gmail.com>
2023-11-11 16:31:13 +0300

    fear: updated the Doctor repository

--- hospital-management/doctor-service/internal/doctor/entity/doctor.go
+++ hospital-management/doctor-service/internal/doctor/entity/doctor.go
@@ -1,14 +1,16 @@
 package entity
 
 import (
+	"errors"
+
 	"github.com/google/uuid"
 )
 
 type DoctorRepository interface {
 	Create(doctor *Doctor) error
 	FindAll() ([]*Doctor, error)
-	Update(doctor *Doctor) Doctor
-	Delete(id string) error
+	Update(doctor *Doctor) error
+	DeleteByID(id string) error
 	GetByID(id string) (*Doctor, error)
 }
 
@@ -33,4 +35,40 @@ func NewDoctor(name, bi, specialty, email, phoneNumber string, experience int) *
 		Email:       email,
 		PhoneNumber: phoneNumber,
 	}
+}
+
+func (d *Doctor) Update(name, bi, specialty, email, phoneNumber string, experience int) {
+	d.Name = name
+	d.BI = bi
+	d.Specialty = specialty
+	d.Experience = experience
+	d.Email = email
+	d.PhoneNumber = phoneNumber
+}
+
+type InMemoryDoctorRepository struct {
+	doctors map[string]*Doctor
+}
+
+func NewInMemoryDoctorRepository() *InMemoryDoctorRepository {
+	return &InMemoryDoctorRepository{
+		doctors: make(map[string]*Doctor),
+	}
+}
+
+func (r *InMemoryDoctorRepository) DeleteByID(id string) error {
+	if _, exists := r.doctors[id]; !exists {
+		return errors.New("doctor not found")
+	}
+
+	delete(r.doctors, id)
+	return nil
+}
+
+func (r *InMemoryDoctorRepository) FindAll() ([]*Doctor, error) {
+	var allDoctors []*Doctor
+	for _, doctor := range r.doctors {
+		allDoctors = append(allDoctors, doctor)
+	}
+	return allDoctors, nil
 }
